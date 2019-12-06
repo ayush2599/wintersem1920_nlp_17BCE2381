51e52b0cbcad4340e814a918f3b950270bddd5bd
ayush2599 <48480745+ayush2599@users.noreply.github.com>
2019-12-06 17:47:33 +0530

    Add files via upload

---
+++ Strings_6_Dec.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+str=input("Enter the string: ")
+
+
+# In[4]:
+
+
+print (str)
+
+
+# In[7]:
+
+
+str='William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.'
+
+
+# In[8]:
+
+
+str='''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.'''
+
+
+# In[9]:
+
+
+print (str)
+
+
+# In[10]:
+
+
+str='''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor,
+widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He
+is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works,
+including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other 
+verses, some of uncertain authorship. His plays have been translated into every major living language and are 
+performed more often than those of any other playwright.'''
+
+
+# In[11]:
+
+
+str='''शेक्सपियर में अत्यंत उच्च कोटि की सर्जनात्मक प्रतिभा थी और साथ ही उन्हें कला के नियमों का सहज ज्ञान भी था। प्रकृति से उन्हें मानो वरदान मिला था अत: उन्होंने जो कुछ छू दिया वह सोना हो गया। उनकी रचनाएँ न केवल अंग्रेज जाति के लिए गौरव की वस्तु हैं वरन् विश्ववांमय की भी अमर विभूति हैं। शेक्सपियर की कल्पना जितनी प्रखर थी उतना ही गंभीर उनके जीवन का अनुभव भी था। अत: जहाँ एक ओर उनके नाटकों तथा उनकी कविताओं से आनंद की उपलब्धि होती है वहीं दूसरी ओर उनकी रचनाओं से हमको गंभीर जीवनदर्शन भी प्राप्त होता है। विश्वसाहित्य के इतिहास में शेक्सपियर के समकक्ष रखे जानेवाले विरले ही कवि मिलते हैं।'''
+
+
+# In[12]:
+
+
+print (str)
+
+
+# In[13]:
+
+
+str="शेक्सपियर में अत्यंत उच्च कोटि की सर्जनात्मक प्रतिभा थी और साथ ही उन्हें कला के नियमों का सहज ज्ञान भी था। प्रकृति से उन्हें मानो वरदान मिला था अत: उन्होंने जो कुछ छू दिया वह सोना हो गया। उनकी रचनाएँ न केवल अंग्रेज जाति के लिए गौरव की वस्तु हैं वरन् विश्ववांमय की भी अमर विभूति हैं। शेक्सपियर की कल्पना जितनी प्रखर थी उतना ही गंभीर उनके जीवन का अनुभव भी था। अत: जहाँ एक ओर उनके नाटकों तथा उनकी कविताओं से आनंद की उपलब्धि होती है वहीं दूसरी ओर उनकी रचनाओं से हमको गंभीर जीवनदर्शन भी प्राप्त होता है। विश्वसाहित्य के इतिहास में शेक्सपियर के समकक्ष रखे जानेवाले विरले ही कवि मिलते हैं।"
+
+
+# In[14]:
+
+
+print(str)
+
+
+# In[ ]:
+
+
+
+
